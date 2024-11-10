b7b7bf396e100a2d80d34f46543c5fb0431b9900
Syaqina Octavia Rizha <syaqinaoctavia@gmail.com>
2024-11-10 22:48:26 +0700

    Update Tubes Pengkom - Auto Gate Toll.py
    hii hii ^-^
    ini kode dari kita berlima
    19624077 Syaqina Octavia Rizha
    19624087 Danya Damaris
    19624122 Muhammad Ghiffary Alfathan
    19624132 Muhammad Daffa Arrizki Yanma 
    19624135 Mishael Gilland

--- Tubes Pengkom - Auto Gate Toll.py
+++ Tubes Pengkom - Auto Gate Toll.py
@@ -1,5 +1,4 @@
 #~~~AUTO GATE TOL~~~#
-
 ''' 
 Deskripsi : Sistem tol yang digunakan adalah sistem tol tertutup. Pengguna tidak perlu membayar ketika
 tap e-Toll di gerbang masuk. Pengguna membayar tol di gerbang keluar, dengan tarif yang dihitung per kilometer 
@@ -9,40 +8,39 @@ Algoritma ini dibuat semirip mungkin dengan gerbang tol yang sebenarnya, yang bi
 Sehingga algoritma ini bisa menginput sebanyak mungkin data kendaraan yang masuk, dan menyimpan data tersebut 
 agak kendaraan bisa keluar di gerbang keluar
 '''
+
 #~~~~~~~KAMUS~~~~~~~#
 '''
-id_eToll : array integer
-in_gerbang_masuk : array string
-in_gerbang_keluar : string
-data_gerbang
-golongan_kendaraan
-
 *~~~Variabel Simpan~~~*
-data_id_eToll
-id_eToll
-save_masuk
-data_gerbang
-km_gerbang
-tarif_per_km
-tarif_golongan
+data_id_eToll : array Integer -> menyimpan data id eToll yang telah digunakan untuk tap masuk
+save_masuk : array string -> menyimpan data gerbang masuk dari kendaraan, indeks disesuaikan dengan data_id_eToll
+data_gerbang : array string -> ada gerbang 'A', 'B', 'C', 'D', 'E'
+km_gerbang : array integer -> letak kilometer dari setiap gerbang, indeks disesuaikan dengan data_gerbang
+tarif_golongan : array integer -> perbandingan harga tiap golongan, dengan golongan 1 menjadi acuan
+tarif_per_km : integer -> harga per kilometer
 
 *~~Variabel Sementara~~*
-km_masuk
-km_keluar
-saldo
-golongan
-count
-indeks 
-masui
-keluar
-status
+(variabel yang nilainya terus berganti ketika ada input baru)
+id_eToll : integer, 5 digit
+km_masuk : integer
+km_keluar : integer
+golongan : string
+gerbang_masuk : string
+gerbang_keluar : string
+saldo : integer
+total_harga : integer
+count : integer -> menghitung indeks array data_id_eToll, karena array tersebut dapat bertambah indeksnya
+status : string -> input : "masuk" atau "keluar"
+tanya : string -> input : "lanjut" atau "tidak"
+lanjut : boolean -> menentukan kapan program akan berhenti
 
 *~~Variabel Bantu~~*
-
-
+cek : boolean -> mengecek array yang kosong
+i : integer -> untuk while loop
 '''
 
-#~~~~DATABASE~~~~#
+#~~~~~DATABASE~~~~~#
+'''membuat data yang akan dijadikan acuan untuk perhitungan'''
 
 data_gerbang = [ '0' for i in range (5) ]
 km_gerbang = [ '0' for i in range (5) ]
@@ -68,37 +66,41 @@ tarif_golongan[5] = 2
 
 tarif_per_km = 700
 
-#~~~~Initialize~~~~#
+#~~~~INITIALIZE~~~~#
+'''inisialisasi variabel yang akan digunakan'''
 
 data_id_eToll = [0]
 save_masuk = ['0']
 saldo = 0
-count = 0
-indeks = 0
 golongan = 0
 total_harga = 0
+count = 0
 lanjut = True
 
+#~~~~~ALGORITHM~~~~~#
 while (lanjut == True):
     status = input("Mau masuk/keluar tol? ")
 
     if(status == 'masuk'):
         #~~Gerbang Masuk~~#
+        '''pengendara akan tap kartu eToll, mesin akan menyimpan data id eToll tersebut, 
+        dan di gerbang mana kartu tersebut ditap'''
         id_eToll = int(input("Masukkan 5 digit ID e-Toll : "))
-        masuk = input("Dari gerbang mana? : ")
+        gerbang_masuk = input("Dari gerbang mana? : ")
         cek = False
         i = 0
 
-        while (i<=count and cek == False):
-            if(data_id_eToll[i]== 0):
+        while (i<=count and cek == False): 
+            if(data_id_eToll[i]== 0): #mengecek indeks yang kosong untuk menyimpan data baru
                 data_id_eToll[i] = id_eToll
-                save_masuk[i] = masuk
+                save_masuk[i] = gerbang_masuk
                 cek = True
             i += 1
-        if(cek == False):
+        if(cek == False): #jika semua indeks terisi, maka indeks bertambah 1, data disimpan di indeks paling akhir
             data_id_eToll.append(id_eToll)
-            save_masuk.append(masuk)
-            count += 1
+            save_masuk.append(gerbang_masuk)
+            count += 1 #count bertambah untuk menyimpan jumlah indeks yang baru
+            
         tanya = str(input("Mau lanjut/tidak? "))
         if(tanya == "tidak"):
             lanjut = False
@@ -109,38 +111,41 @@ while (lanjut == True):
         i = 0
         cek = False
         while (i<=count and cek == False):
+            #cek data id eToll, apakah kartu tersebut digunakan untuk tap masuk?
             if(data_id_eToll[i] == id_eToll):
-                indeks = i
                 saldo = int(input("Masukkan nominal saldo anda : "))
-                keluar = input("Keluar di gerbang mana? : ")
+                gerbang_keluar = input("Keluar di gerbang mana? : ")
                 golongan = int(input("Masukkan golongan kendaraan : "))
                 for j in range(5):
-                    if(data_gerbang[j] == save_masuk[indeks]):
+                    if(data_gerbang[j] == save_masuk[i]):
                         km_masuk = km_gerbang[j]
                         break
                 for j in range(5):
-                    if(data_gerbang[j] == keluar):
+                    if(data_gerbang[j] == gerbang_keluar):
                         km_keluar = km_gerbang[j]
                         break
                 cek = True
             i += 1    
-            if(i>count and cek == False):
+            if(i>count and cek == False): #kartu yang bisa digunakan hanyalah kartu yang telah digunakan untuk tap masuk
                 print("Kartu tidak bisa digunakan")
                 id_eToll = int(input("Masukkan 5 digit ID e-Toll : "))            
-                i = 0
+                i = 0 #pengendara harus menggunakan kartu lain, i kembali menjadi 0, loop untuk cek id eToll akan diulang kembali
         total_harga = tarif_per_km * abs(km_keluar-km_masuk) * tarif_golongan[golongan]
+        #untuk menghitung harga yang harus dibayar digunakan absolut karena diasumsikan di setiap tempat ada gerbang masuk dan keluar
 
         if(saldo < total_harga):
-            while(saldo < total_harga):
+            while(saldo < total_harga): 
+                #jika saldo tidak mencukupi, pengendara harus top up saldo terlebih dahulu, sehingga akan diinput saldo baru
                 print("Saldo tidak mencukupi")
                 print("Harga yang harus dibayar adalah: Rp" + str(total_harga))
                 saldo = int(input("Masukkan nominal saldo baru: "))
         if(saldo >= total_harga):
             print("Silakan melanjutkan perjalanan :D")
             print("Sisa saldo anda Rp" + str(saldo - total_harga))
+            
             #~~~~hapus data id dan gerbang yang tersimpan~~~~#
-            data_id_eToll[indeks] = 0
-            save_masuk[indeks] = '0'
+            data_id_eToll[i-1] = 0
+            save_masuk[i-1] = '0'
         
         tanya = str(input("Mau lanjut/tidak? "))
         if(tanya == "tidak"):
